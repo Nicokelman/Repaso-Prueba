d30c8d7c44fd8ba2f713d9d8923b5ea80ff99d25
KELMAN NICOLAS <47297850@ort>
2022-04-07 08:59:18 -0300

    Realizado Ej 1 y 2

---
+++ Assets/Repaso/Ej01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour
+{
+   public int num1;
+   public int num2;
+   
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Repaso/Ej02.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    public int prod1;
+    public int prod2;
+    public int prod3;
+    public int monto;
+
+    void Start()
+    {
+        int suma;
+        suma = prod1 + prod2 + prod3;
+        if (suma > monto)
+        {
+            Debug.Log("La suma de los 3 productos supera el dinero disponible");
+            int resto;
+            resto = suma - monto;
+            Debug.Log("Lo que falta es " + resto);
+        }
+        else
+        {
+            Debug.Log("La suma de los 3 productos no supera el dinero dispnible");
+            int sobra;
+            sobra = monto - suma;
+            Debug.Log("Lo que sobra es " + sobra);
+        }
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
